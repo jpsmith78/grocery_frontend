d17eef1c01d64237af821f877104ebf5b8c899e5
Justin Smith <justinsmith@Justins-MacBook-Pro.local>
2019-03-14 17:44:03 -0400

    added many to one data to page

--- src/components/FridgeItem.js
+++ src/components/FridgeItem.js
@@ -8,7 +8,9 @@ class FridgeItem extends Component {
 
         <div className="fridge_item">
           {this.props.item.fridge_item}<br/>
-          {this.props.item.fridge_category}
+          {this.props.item.fridge_category}<br/>
+          {this.props.item.fridge_quantity}<br/>
+          {this.props.item.fridge_unit}
         </div>
 
         <div className="fridge_action">
--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -10,6 +10,19 @@ class ListItem extends Component {
           {this.props.item.item}<br/>
           {this.props.item.category}<br/>
           {this.props.item.price}<br/>
+          {this.props.item.quantity}<br/>
+          {this.props.item.unit}<br/>
+          {this.props.item.have_at_home ?
+            this.props.item.have_at_home.map((fridge_item, index)=>{
+              return(
+                <ul key={index}>
+                  <li>{fridge_item.fridge_item}</li>
+                  <li>{fridge_item.fridge_category}</li>
+                  <li>{fridge_item.fridge_quantity}</li>
+                  <li>{fridge_item.fridge_unit}</li>
+                </ul>
+              )
+            }) : ""}
         </div>
 
         <div className="list_action">
